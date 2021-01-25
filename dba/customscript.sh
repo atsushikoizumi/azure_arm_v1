7e3c307527cdb0f85f680b9d9e59c324e2db3d48
emilymacpro <attsun1031@gmail.com>
2021-01-25 20:46:16 +0900

    update for CentOS

--- dba/customscript.sh
+++ dba/customscript.sh
@@ -2,7 +2,6 @@
 
 USERNAME=$1
 LOGFILE=/var/log/customscript.log
-SCRIPTDIR=$(cd $(dirname $0); pwd)
 
 # start
 touch $LOGFILE
@@ -10,35 +9,37 @@ whoami                    >> $LOGFILE
 pwd                       >> $LOGFILE
 echo "USERNAME=$USERNAME" >> $LOGFILE
 
-# Label name is different for each deployment.
-lsblk -o NAME,TYPE | grep disk > ${SCRIPTDIR}/lsblk.txt
-while read line || [ -n "${line}" ];
-do
-    array=(${line})
-    if [ "`df | grep -e "${array[0]}" | grep -e "boot"`" ]; then
-        echo "${array[0]} is os_disk."   >> $LOGFILE
-        OSDISK="${array[0]}"
-    fi
-    if [ ! "`df | grep -e "${array[0]}"`" ]; then
-        echo "${array[0]} is data_disk." >> $LOGFILE
-        DATADISK="${array[0]}"
-    fi
-done < ${SCRIPTDIR}/lsblk.txt
-
-# disck setting
-sgdisk -e /dev/${OSDISK}                                               >> $LOGFILE 2>&1
-lsblk                                                                  >> $LOGFILE 2>&1
-parted -s /dev/${OSDISK}   -- mkpart lvmrootvg xfs 68.7GB  100%        >> $LOGFILE 2>&1
-parted -s /dev/${DATADISK} -- mklabel gpt mkpart lvmrootvg xfs 0% 100% >> $LOGFILE 2>&1
-lsblk                                                                  >> $LOGFILE 2>&1
-pvcreate /dev/${OSDISK}3 /dev/${DATADISK}1                             >> $LOGFILE 2>&1
-vgextend rootvg /dev/${OSDISK}3                                        >> $LOGFILE 2>&1
-vgextend rootvg /dev/${DATADISK}1                                      >> $LOGFILE 2>&1
-lvextend -L +100G /dev/rootvg/homelv                                   >> $LOGFILE 2>&1
-xfs_growfs /dev/mapper/rootvg-homelv                                   >> $LOGFILE 2>&1
-lsblk                                                                  >> $LOGFILE 2>&1
-pvdisplay                                                              >> $LOGFILE 2>&1
-vgdisplay                                                              >> $LOGFILE 2>&1
+## for RHEL disk setting
+## Label name is different for each deployment.
+#SCRIPTDIR=$(cd $(dirname $0); pwd)
+#lsblk -o NAME,TYPE | grep disk > ${SCRIPTDIR}/lsblk.txt
+#while read line || [ -n "${line}" ];
+#do
+#    array=(${line})
+#    if [ "`df | grep -e "${array[0]}" | grep -e "boot"`" ]; then
+#        echo "${array[0]} is os_disk."   >> $LOGFILE
+#        OSDISK="${array[0]}"
+#    fi
+#    if [ ! "`df | grep -e "${array[0]}"`" ]; then
+#        echo "${array[0]} is data_disk." >> $LOGFILE
+#        DATADISK="${array[0]}"
+#    fi
+#done < ${SCRIPTDIR}/lsblk.txt
+#
+## disck setting
+#sgdisk -e /dev/${OSDISK}                                               >> $LOGFILE 2>&1
+#lsblk                                                                  >> $LOGFILE 2>&1
+#parted -s /dev/${OSDISK}   -- mkpart lvmrootvg xfs 68.7GB  100%        >> $LOGFILE 2>&1
+#parted -s /dev/${DATADISK} -- mklabel gpt mkpart lvmrootvg xfs 0% 100% >> $LOGFILE 2>&1
+#lsblk                                                                  >> $LOGFILE 2>&1
+#pvcreate /dev/${OSDISK}3 /dev/${DATADISK}1                             >> $LOGFILE 2>&1
+#vgextend rootvg /dev/${OSDISK}3                                        >> $LOGFILE 2>&1
+#vgextend rootvg /dev/${DATADISK}1                                      >> $LOGFILE 2>&1
+#lvextend -L +100G /dev/rootvg/homelv                                   >> $LOGFILE 2>&1
+#xfs_growfs /dev/mapper/rootvg-homelv                                   >> $LOGFILE 2>&1
+#lsblk                                                                  >> $LOGFILE 2>&1
+#pvdisplay                                                              >> $LOGFILE 2>&1
+#vgdisplay                                                              >> $LOGFILE 2>&1
 
 ### yum update
 yum update -y                                                          >> $LOGFILE 2>&1
@@ -62,13 +63,11 @@ yum install -y postgresql postgresql-server                            >> $LOGFI
 mkdir /opt/oracle                                                      >> $LOGFILE 2>&1
 curl https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basic-linuxx64.rpm -o /opt/oracle/oracle-instantclient-basic-linuxx64.rpm      >> $LOGFILE 2>&1
 curl https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-sqlplus-linuxx64.rpm -o /opt/oracle/oracle-instantclient-sqlplus-linuxx64.rpm  >> $LOGFILE 2>&1
-sleep 5
 yum install -y /opt/oracle/oracle-instantclient-basic-linuxx64.rpm     >> $LOGFILE 2>&1
 yum install -y /opt/oracle/oracle-instantclient-sqlplus-linuxx64.rpm   >> $LOGFILE 2>&1
 echo 'export NLS_LANG=Japanese_Japan.AL32UTF8' >> /home/${USERNAME}/.bash_profile           >> $LOGFILE 2>&1
 
 ### sqlcmd
-curl https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/msprod.repo  >> $LOGFILE 2>&1
-sleep 5
+curl https://packages.microsoft.com/config/rhel/8/prod.repo -o /etc/yum.repos.d/msprod.repo >> $LOGFILE 2>&1
 ACCEPT_EULA=Y yum install -y mssql-tools                                                    >> $LOGFILE 2>&1
 echo 'export PATH=$PATH:/opt/mssql-tools/bin' >> /home/${USERNAME}/.bash_profile            >> $LOGFILE 2>&1
